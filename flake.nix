c2d5b92b73975af0466c8b3bc17dd135c1b93eab
Sef <39380372+s-e-f@users.noreply.github.com>
2024-06-18 21:52:57 +0200

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
     nixvim = {
       url = "github:nix-community/nixvim";
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
       url = "github:nix-community/home-manager/master";
