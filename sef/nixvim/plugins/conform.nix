a5b8eff51a332439b0fbed4027b6637093a255d5
Sef <39380372+s-e-f@users.noreply.github.com>
2024-10-08 10:55:22 +0200

    Nixvim: conform

--- sef/nixvim/plugins/conform.nix
+++ sef/nixvim/plugins/conform.nix
@@ -1,4 +1,18 @@
 { pkgs, config, ... }:
 {
-  # TODO: Conform
+  programs.nixvim.plugins.conform-nvim = {
+    enable = true;
+    settings = {
+      format_on_save = {
+        timeout_ms = 500;
+        lsp_format = "fallback";
+      };
+      formatters_by_ft = {
+        lua = [ "stylua" ];
+        java = [ "google-java-format" ];
+        nix = [ "nixfmt" ];
+        cs = [ "csharpier" ];
+      };
+    };
+  };
 }
