0474686bdc3e3ac0ce0a4a51c72b13d29a4885c9
Sef <39380372+s-e-f@users.noreply.github.com>
2024-10-11 15:30:04 +0200

    Waybar styling

--- sef/hypr/waybar.nix
+++ sef/hypr/waybar.nix
@@ -129,47 +129,68 @@
       format-alt = "{ifname}: {ipaddr}/{cidr}";
     };
   };
-  programs.waybar.style = ''
+  stylix.targets.waybar.enable = false;
+  programs.waybar.style = with config.lib.stylix.colors.withHashtag; ''
+    @define-color base00 ${base00};
+    @define-color base01 ${base01};
+    @define-color base02 ${base02};
+    @define-color base03 ${base03};
+    @define-color base04 ${base04};
+    @define-color base05 ${base05};
+    @define-color base06 ${base06};
+    @define-color base07 ${base07};
+    @define-color base08 ${base08};
+    @define-color base09 ${base09};
+    @define-color base0A ${base0A};
+    @define-color base0B ${base0B};
+    @define-color base0C ${base0C};
+    @define-color base0D ${base0D};
+    @define-color base0E ${base0E};
+    @define-color base0F ${base0F};
     * {
-        border: none;
-        border-radius: 0;
-        font-family: 'Noto Sans', 'Font Awesome 6 Free', 'Font Awesome 6 Brands', Roboto, Helvetica, Arial, sans-serif;
-        font-weight: bold;
-        font-size: 14px;
-        min-height: 0;
+      border: none;
+      border-radius: 0;
+      font-family: 'Noto Sans', 'Font Awesome 6 Free', 'Font Awesome 6 Brands', Roboto, Helvetica, Arial, sans-serif;
+      font-weight: bold;
+      font-size: 14px;
+      min-height: 0;
+    }
+
+    window#waybar, tooltip {
+      background: alpha(@base00, ${with config.stylix.opacity; builtins.toString desktop});
+      color: @base05;
     }
 
     window#waybar>box {
-        border-radius: 0px;
-        padding: 10px;
+      border-radius: 0px;
+      padding: 10px;
     }
 
-    #workspaces button {
-        padding: 0 5px;
-        background: transparent;
-        color: inherit;
+    .modules-left #workspaces button {
+      padding: 0 5px;
+      background: transparent;
+      color: inherit;
     }
 
     #workspaces button.active {
-        color: @accent_color;
-        border-bottom: 0px solid transparent;
+      color: @accent_color;
     }
 
     @keyframes blink {
-        to {
-            background-color: @base06;
-            color: @base01;
-        }
+      to {
+        background-color: @base06;
+        color: @base01;
+      }
     }
 
     #battery.warning:not(.charging) {
-        background: @base08;
-        color: @base06;
-        animation-name: blink;
-        animation-duration: 0.5s;
-        animation-timing-function: steps(12);
-        animation-iteration-count: infinite;
-        animation-direction: alternate;
+      background: @base08;
+      color: @base06;
+      animation-name: blink;
+      animation-duration: 0.5s;
+      animation-timing-function: steps(12);
+      animation-iteration-count: infinite;
+      animation-direction: alternate;
     }
   '';
 }
