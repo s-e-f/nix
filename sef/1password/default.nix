48003d8ffd8e56084b52dd45dea1d8965de42ce9
Sef <39380372+s-e-f@users.noreply.github.com>
2024-11-19 10:29:39 +0100

    ssh settings

--- sef/1password/default.nix
+++ sef/1password/default.nix
@@ -3,13 +3,18 @@
   programs = {
     ssh = {
       enable = true;
+      extraConfig = ''
+        IdentityAgent ~/.1password/agent.sock
+      '';
+      addKeysToAgent = "yes";
+      compression = true;
+      controlMaster = "auto";
+      controlPath = "~/.ssh/control-%C";
+      controlPersist = "300";
       matchBlocks."github.com" = {
         hostname = "ssh.github.com";
         port = 443;
         user = "git";
-        extraOptions.IdentityAgent = "~/.1password/agent.sock";
-      };
-      matchBlocks."*" = {
         extraOptions.IdentityAgent = "~/.1password/agent.sock";
       };
     };
