6b2a55c9b160852e2fe455d34c207c57f96de9ee
Sef <39380372+s-e-f@users.noreply.github.com>
2024-12-03 16:06:38 +0100

    nix flake update

--- sef/ssh/default.nix
+++ sef/ssh/default.nix
@@ -8,6 +8,11 @@
       controlMaster = "auto";
       controlPath = "~/.ssh/control-%C";
       controlPersist = "300";
+      matchBlocks."github.com" = {
+        hostname = "ssh.github.com";
+        port = 443;
+        user = "git";
+      };
     };
   };
 }
