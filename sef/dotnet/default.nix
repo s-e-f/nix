c2dc5a5e014fdaeea9faa56ea076c4141455bb3c
Sef <39380372+s-e-f@users.noreply.github.com>
2025-01-17 00:29:13 +0100

    nix flake update

--- sef/dotnet/default.nix
+++ sef/dotnet/default.nix
@@ -1,13 +1,15 @@
 {
+  pkgs,
   ...
 }:
 {
   # Install dotnet manually into the home folder to allow tool and workload installations to work
   programs.zsh.envExtra = ''
     export DOTNET_ROOT="$HOME/.dotnet"
-    export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT="1"
     export DOTNET_CLI_TELEMETRY_OPTOUT="1"
     export DOTNET_SKIP_FIRST_TIME_EXPERIENCE="true"
     export PATH="$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.icu}/lib:${pkgs.openssl}/lib"
+    export LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
   '';
 }
